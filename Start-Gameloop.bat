8438f9cc4ea90218d69f0042600e75ccd3d3a76c
Frazix12 <71058095+Frazix12@users.noreply.github.com>
2022-09-01 14:18:37 +0600

    Update Start-Gameloop.bat

--- Start-Gameloop.bat
+++ Start-Gameloop.bat
@@ -1,7 +1,22 @@
-echo off
-chcp 65001 >nul
-mode con lines=20 cols=50
-color 3
-title PUBG - Multi Tool ^
+color A
+@echo off
+taskkill /F /IM adb.exe
+taskkill /F /IM AndroidProcess.exe
+taskkill /F /IM ProjectTitan.exe
+TaskKill /F /IM appmarket.exe
+TaskKill /F /IM AndroidEmulator.exe
+TaskKill /F /IM AndroidEmulatorEn.exe
+TaskKill /F /IM AndroidEmulatorEx.exe
+TaskKill /F /IM aow_exe.exe
+TaskKill /F /IM QMEmulatorService.exe
+taskkill /f /im RuntimeBroker.exe
+taskkill /f /im adb.exe
+taskkill /f /im GameLoader.exe
+taskkill /f /im TSettingCenter.exe
+taskkill /f /im checker.exe
+net stop aow_drv
+net stop Tensafe
+SC STOP aow_drv
+SC DELETE aow_drv
 
 "C:\Program Files\TxGameAssistant\ui\AndroidEmulatorEx.exe" -vm 100
